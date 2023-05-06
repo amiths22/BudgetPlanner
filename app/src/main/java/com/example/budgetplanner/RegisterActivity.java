2efa80c44a1f10c2174c6c4c5132c26e59e46b8e
Amith Satyanarayan <amith.s22@gmail.com>
2023-05-06 01:35:52 -0600

    Google signin and firebase Email completed

--- app/src/main/java/com/example/budgetplanner/RegisterActivity.java
+++ app/src/main/java/com/example/budgetplanner/RegisterActivity.java
@@ -4,11 +4,19 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -18,6 +26,12 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText passwordEditText;
     private EditText confirmPasswordEditText;
     private Button registerButton;
+    private Button Btn;
+    private ProgressBar progressbar;
+    private FirebaseAuth mAuth;
+    private EditText emailTextView, passwordTextView;
+
+
 
     private SQLiteDatabase database;
 
@@ -35,7 +49,7 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton = findViewById(R.id.registerButton);
 
         // Open the database
-        database = openOrCreateDatabase("userDB", MODE_PRIVATE, null);
+       /* database = openOrCreateDatabase("userDB", MODE_PRIVATE, null);
 
         // Create the "users" table if it doesn't exist
         database.execSQL("CREATE TABLE IF NOT EXISTS users (name TEXT, username TEXT, password TEXT)");
@@ -66,7 +80,97 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
+        });*/
+        mAuth = FirebaseAuth.getInstance();
+
+        // initialising all views through id defined above
+       emailTextView = findViewById(R.id.usernameEditText);
+        passwordTextView = findViewById(R.id.passwordEditText);
+        Btn = findViewById(R.id.btnregister);
+        progressbar = findViewById(R.id.progressbar);
+
+        // Set on Click Listener on Registration button
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                String password = passwordEditText.getText().toString();
+                String confirmPassword = confirmPasswordEditText.getText().toString();
+                if (!password.equals(confirmPassword)) {
+                    Toast.makeText(RegisterActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else
+                registerNewUser();
+            }
         });
+    }
+
+    private void registerNewUser()
+    {
+
+        // show the visibility of progress bar to show loading
+        progressbar.setVisibility(View.VISIBLE);
+
+        // Take the value of two edit texts in Strings
+        String email, password;
+        email = emailTextView.getText().toString();
+        password = passwordTextView.getText().toString();
+
+        // Validations for input email and password
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter email!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter password!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        // create new user or register new user
+        mAuth
+                .createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(),
+                                            "Registration successful!",
+                                            Toast.LENGTH_LONG)
+                                    .show();
+
+                            // hide the progress bar
+                            progressbar.setVisibility(View.GONE);
+
+                            // if the user created intent to login activity
+                            Intent intent
+                                    = new Intent(RegisterActivity.this,
+                                    LoginActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+
+                            // Registration failed
+                            Toast.makeText(
+                                            getApplicationContext(),
+                                            "Registration failed!!"
+                                                    + " Please try again later",
+                                            Toast.LENGTH_LONG)
+                                    .show();
+
+                            // hide the progress bar
+                            progressbar.setVisibility(View.GONE);
+                        }
+                    }
+                });
     }
 
     @Override
